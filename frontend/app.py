a80ca762f3b5cdb9eced7b0030b9bf805373638f
Anvesh Kartan <vaishnavi.kubeer@gmail.com>
2025-03-12 15:38:00 +0530

    added display tabs upon success login

--- frontend/app.py
+++ frontend/app.py
@@ -10,31 +10,36 @@ USERNAME = st.secrets["USERNAME"]
 PASSWORD = st.secrets["PASSWORD"]
 
 # Login form
-st.title("Login to Access")
+st.title("Login to Access Expense Management system")
 entered_user = st.text_input("Username")
 entered_pass = st.text_input("Password", type="password")
 login_button = st.button("Login")
+login_success=False
 
 if login_button:
     if entered_user == USERNAME and entered_pass == PASSWORD:
         st.success("Login Successful!")
+        login_success=True
         st.experimental_set_query_params(auth="true")
     else:
         st.error("Invalid credentials")
 
-#API_URL="http://localhost:8000"
-API_URL="https://expense-tracker-g6xy.onrender.com"
 
-st.title("Expense Management System")
+if login_success:
+    # API_URL="http://localhost:8000"
+    API_URL = "https://expense-tracker-g6xy.onrender.com"
 
-tab1,tab2,tab3=st.tabs(["Add/Update","Analytics By Category","Analytics By Month"])
+    st.title("Expense Management System")
 
-with tab1:
-    add_update_tab()
-with tab2:
-    analytics_by_Category_tab()
-with tab3:
-    analytics_by_month_tab()
+    tab1, tab2, tab3 = st.tabs(["Add/Update", "Analytics By Category", "Analytics By Month"])
+
+    with tab1:
+        add_update_tab()
+    with tab2:
+        analytics_by_Category_tab()
+    with tab3:
+        analytics_by_month_tab()
+
 
 
 
