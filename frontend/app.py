c0927c2dd1bbdd8fee25539f075a9166fb7be311
Vaishnavi <vaishnavi.kubeer@gmail.com>
2025-03-15 20:09:46 +0530

    removing bg color

--- frontend/app.py
+++ frontend/app.py
@@ -12,14 +12,10 @@ def apply_custom_css():
     st.markdown(
         """
         <style>
-        .stApp { background-color: #959bde; }
+        
         html, body, [class*="st-"] { font-family: 'Poppins', sans-serif; }
         h1, h2, h3 { color: #FF5733; text-align: center; }
         div.stButton > button:first-child { background-color: #4CAF50; color: white; border-radius: 8px; }
-        [data-testid="stSidebar"] { background-color: #EAEAEA; }
-        [data-testid="stSidebar"] {
-            background-color: #959bde !important;  /* Light Gray */
-        }
 
         /* Add Padding & Rounded Corners to Sidebar */
         [data-testid="stSidebar"] > div {
